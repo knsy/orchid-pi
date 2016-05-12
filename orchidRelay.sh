b18503cbc131ee85adce0adbb9033de93c84439a
knsy <ksy@yakovenko.net>
2016-05-11 21:32:31 -0700

    Added cron information

--- orchidRelay.sh
+++ orchidRelay.sh
@@ -2,6 +2,12 @@
 #A script to turn on and off the relay connected to over gpio
 #based on a schedule.
 
+#add to cron tab.
+#minute (0-59), hour (0-23, 0 = midnight), day (1-31), month (1-12), weekday (0-6, 0 = Sunday), command 
+#01 09 * * 1, /path/to/script
+#runs once a week, on monday at 9am.
+#https://help.ubuntu.com/community/CronHowto
+
 #pin 11 on rpi gpio0
 echo "17" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio17/direction
